1e193736d96bdc11579cb227d4018791bc8a99c7
Bob Wan <bob@bob-HP-Compaq-6200-Pro-MT-PC.(none)>
2014-07-14 18:03:47 +0800

    add config

--- api/api.go
+++ api/api.go
@@ -13,12 +13,3 @@ type TokenResponse struct {
 	Uid   int64
 	Token string
 }
-
-func response(code int, data interface{}) *Response {
-	r := &Response{
-		code,
-		data,
-	}
-
-	return r
-}
--- missled/config.go
+++ missled/config.go
@@ -1,6 +1,25 @@
 package main
 
-const (
-	DSN = "root:apple@tcp(localhost:3306)/auth?charset=utf8"
-	MC  = "localhost:11211"
+import (
+	"github.com/BurntSushi/toml"
+	"log"
 )
+
+const (
+// DSN = "root:apple@tcp(localhost:3306)/auth?charset=utf8"
+// MC  = "localhost:11211"
+)
+
+type Config struct {
+	MysqlDsn    string
+	MemcacheDsn string
+}
+
+func loadConfig() (config Config, err error) {
+	if _, err = toml.DecodeFile("config.toml", &config); err != nil {
+		log.Printf("read config file error: %s\n", err.Error())
+		return
+	}
+
+	return
+}
