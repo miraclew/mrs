b972b8299032eab8b766200b97b7d7228d718add
miraclew <miraclew@gmail.com>
2014-07-24 22:40:13 +0800

    aa

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #
 
-go build github.com/miraclew/mrs/util
-go build github.com/miraclew/mrs/missle
-go build github.com/miraclew/mrs/api
-go build github.com/miraclew/mrs/push
-go build github.com/miraclew/mrs/missled
+#go build github.com/miraclew/mrs/util
+#go build github.com/miraclew/mrs/missle
+#go build github.com/miraclew/mrs/api
+#go build github.com/miraclew/mrs/push
+go install github.com/miraclew/mrs/missled
