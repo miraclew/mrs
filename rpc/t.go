07a354a919c8c95d77dd682cbc03b0c56a8c1e87
Bob Wan <bob@bob-HP-Compaq-6200-Pro-MT-PC.(none)>
2014-07-30 17:07:26 +0800

    add json rpc

---
+++ rpc/t.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gorilla/rpc"
+	"github.com/gorilla/rpc/json"
+	"net/http"
+)
+
+type HelloArgs struct {
+	Who string
+}
+
+type HelloReply struct {
+	Message string
+}
+
+type HelloService struct{}
+
+func (h *HelloService) Say(r *http.Request, args *HelloArgs, reply *HelloReply) error {
+	reply.Message = "Hello, " + args.Who + "!"
+	return nil
+}
+
+func main() {
+	s := rpc.NewServer()
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	s.RegisterService(new(HelloService), "")
+
+	http.Handle("/rpc", s)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+// curl http://localhost:8080/rpc -H "Content-Type: application/json" -d '{ "method": "HelloService.Say", "params": [{"Who":"JSON-RPC"}], "id": 1}'
+//
+//
