0dc8231f993b62a588ee96a2d0a969c5d6a99b6e
miraclew <miraclew@gmail.com>
2015-03-24 16:20:33 +0800

    add 2 functions

--- util/string.go
+++ util/string.go
@@ -3,6 +3,7 @@ package util
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"sort"
 	"strings"
 )
 
@@ -28,4 +29,35 @@ func Substr(s string, pos, length int) string {
 		l = len(runes)
 	}
 	return string(runes[pos:l])
+}
+
+func RemoveDuplicates(xs *[]string) {
+	found := make(map[string]bool)
+	j := 0
+	for i, x := range *xs {
+		if !found[x] {
+			found[x] = true
+			(*xs)[j] = (*xs)[i]
+			j++
+		}
+	}
+	*xs = (*xs)[:j]
+}
+
+func SplitUniqSort(s string) []string {
+	members := strings.Split(s, ",")
+	var newMembers []string
+	for i := 0; i < len(members); i++ {
+		m := members[i]
+		m = strings.TrimSpace(m)
+		if len(m) <= 0 {
+			continue
+		}
+		newMembers = append(newMembers, m)
+	}
+
+	RemoveDuplicates(&newMembers)
+	sorted := sort.StringSlice(newMembers)
+	sorted.Sort()
+	return sorted
 }
